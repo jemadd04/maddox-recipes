a17bda6c83051b0244a9372a10b2c7345647e859
James Maddox <james.maddox07@gmail.com>
2019-07-23 02:03:07 -0400

    Updates

---
+++ precache-manifest.60fbf43dccdb55a761c0960146cbf4e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f78014882baa7ee8b0d5cb08015fc363",
+    "url": "/maddox-recipes/index.html"
+  },
+  {
+    "revision": "d7e91eed2056c7446887",
+    "url": "/maddox-recipes/static/css/main.2263b912.chunk.css"
+  },
+  {
+    "revision": "573f771124ebfdec98dc",
+    "url": "/maddox-recipes/static/js/2.35493de6.chunk.js"
+  },
+  {
+    "revision": "d7e91eed2056c7446887",
+    "url": "/maddox-recipes/static/js/main.57a47124.chunk.js"
+  },
+  {
+    "revision": "ae3e75db3460f64cc926",
+    "url": "/maddox-recipes/static/js/runtime~main.e0d6b7d0.js"
+  }
+]);
